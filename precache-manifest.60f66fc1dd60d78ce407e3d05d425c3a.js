cef2f442b5c5b1ab2c1f305f474de259afdea33a
abdur-rakib <abdurrakib961@gamil.com>
2020-05-30 10:33:02 +0600

    Updates

---
+++ precache-manifest.60f66fc1dd60d78ce407e3d05d425c3a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e517149c0dcfbf9fcfeb57f6647372c2",
+    "url": "/react-beach-resort-project/index.html"
+  },
+  {
+    "revision": "771653576e19360a74ad",
+    "url": "/react-beach-resort-project/static/css/main.881258fc.chunk.css"
+  },
+  {
+    "revision": "e21ebe79e5b565f30dd1",
+    "url": "/react-beach-resort-project/static/js/2.d874c9b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-beach-resort-project/static/js/2.d874c9b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "771653576e19360a74ad",
+    "url": "/react-beach-resort-project/static/js/main.08a55348.chunk.js"
+  },
+  {
+    "revision": "a9c33b712d8a838b7031",
+    "url": "/react-beach-resort-project/static/js/runtime-main.fb94f8d3.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/react-beach-resort-project/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/react-beach-resort-project/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/react-beach-resort-project/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/react-beach-resort-project/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/react-beach-resort-project/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/react-beach-resort-project/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/react-beach-resort-project/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/react-beach-resort-project/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/react-beach-resort-project/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/react-beach-resort-project/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/react-beach-resort-project/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/react-beach-resort-project/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/react-beach-resort-project/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/react-beach-resort-project/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/react-beach-resort-project/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/react-beach-resort-project/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/react-beach-resort-project/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/react-beach-resort-project/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/react-beach-resort-project/static/media/room-9.7b17c49f.jpeg"
+  }
+]);
